7898e0607a43abcf4964ec91a7f9c9588d6b64a5
Brayan Manzano <al221610173@gmail.com>
2025-05-27 14:35:04 -0600

    new distribution to visual gallery

--- resources/views/front/components/smallSocials.blade.php
+++ resources/views/front/components/smallSocials.blade.php
@@ -3,8 +3,8 @@
         rel="noopener noreferrer" 
         target="_blank"  
         class="uk-icon-link uk-margin-small-right"  
-        uk-icon="icon: facebook;  ratio: 0.8">
+        uk-icon="icon: facebook;  ratio: 1">
     </a>
-    <a href="#" class="uk-icon-link uk-margin-small-right"  uk-icon="icon: instagram; ratio: 0.8"></a>
-    <a href="#" class="uk-icon-link"                        uk-icon="icon: youtube;   ratio: 0.8"></a>
+    <a href="#" class="uk-icon-link uk-margin-small-right"  uk-icon="icon: instagram; ratio: 1"></a>
+    <a href="#" class="uk-icon-link"                        uk-icon="icon: youtube;   ratio: 1"></a>
 </div>
--- resources/views/front/components/visualDiary.blade.php
+++ resources/views/front/components/visualDiary.blade.php
@@ -1,21 +1,27 @@
-<div class="uk-child-width-1-2@s" uk-lightbox="animation: fade" style="display: flex;
-    flex-wrap: wrap;">
-    @foreach ($lastNewPhoto as  $lastPhoto)
-        @foreach ($lastPhoto->images as $key=>  $image)
+<div class="uk-child-width-1-2@s uk-child-width-1-3@m uk-child-width-1-2@l" 
+     uk-lightbox="animation: fade" 
+     uk-grid>
+    @foreach ($lastNewPhoto as $lastPhoto)
+        @foreach ($lastPhoto->images as $key => $image)
             @if ($key >= 0)
-            <div style="padding: 5px;">
-                <a class="uk-inline" 
-                    href="{{ asset('storage'.'/'.$lastPhoto->user->name.'/'.$image->name) }}" 
-                    data-caption="{{ $lastPhoto->title }}">
-                    <img src="{{ asset('storage'.'/'.$lastPhoto->user->name.'/'.$image->name) }}" 
-                            width="1800" 
-                            height="1200" 
-                            class="visual-img uk-border-rounded"
-                            alt="{{ $lastPhoto->title }}">
+            <div>
+                <a class="uk-inline uk-display-block" 
+                   href="{{ asset('storage'.'/'.$lastPhoto->user->name.'/'.$image->name) }}" 
+                   data-caption="{{ $lastPhoto->title }}">
+                    <div class="uk-cover-container uk-border-rounded">
+                        <img src="{{ asset('storage'.'/'.$lastPhoto->user->name.'/'.$image->name) }}" 
+                             class="uk-cover"
+                             loading="eager"
+                             style="object-fit: cover;"
+                             width="1800" 
+                             height="1200" 
+                             alt="{{ $lastPhoto->title }}"
+                             uk-cover>
+                        <canvas width="600" height="400"></canvas>
+                    </div>
                 </a>
             </div>
             @endif
         @endforeach
     @endforeach
 </div>
-
